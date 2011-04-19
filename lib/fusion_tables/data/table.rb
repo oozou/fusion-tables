f6337542fcf8f85ce79edba6a870b12a788e4682
Prem Sichanugrist <s@sikachu.com>
2011-04-19 17:33:37 +0700

    Escape the ROWID in update statement

--- lib/fusion_tables/data/table.rb
+++ lib/fusion_tables/data/table.rb
@@ -92,7 +92,7 @@ module GData
           data = encode([data]).first
           data = data.to_a.map{|x| x.join("=")}.join(", ")
 
-          sql = "UPDATE #{@id} SET #{data} WHERE ROWID = #{row_id}"
+          sql = "UPDATE #{@id} SET #{data} WHERE ROWID = '#{row_id}'"
           GData::Client::FusionTables::Data.parse(@client.sql_post(sql)).body
         end
 
